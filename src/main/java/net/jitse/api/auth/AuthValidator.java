a2ae3d38cba27671676d94be30e6654c3c375335
JitseB <Jitse@operamail.com>
2018-02-05 10:08:13 +0100

    Added validator methods.

--- src/main/java/net/jitse/api/auth/AuthValidator.java
+++ src/main/java/net/jitse/api/auth/AuthValidator.java
@@ -1,4 +1,22 @@
 package net.jitse.api.auth;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class AuthValidator {
+
+    private final MessageDigest messageDigest;
+
+    public AuthValidator() throws NoSuchAlgorithmException {
+        this.messageDigest = MessageDigest.getInstance("SHA-256");
+    }
+
+    public String hash(String input) {
+        messageDigest.update(input.getBytes());
+        return new String(messageDigest.digest());
+    }
+
+    public boolean validate(String hash, String input) {
+        return hash(input).equals(hash);
+    }
 }
