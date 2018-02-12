d75f25a122c3de2294f61c9310d7e8e548982db2
JitseB <Jitse@operamail.com>
2018-02-12 21:38:38 +0100

    Added power mock runner to spigot logger test.

--- src/test/java/net/jitse/phantom/spigot/utilities/logging/SpigotLoggerTest.java
+++ src/test/java/net/jitse/phantom/spigot/utilities/logging/SpigotLoggerTest.java
@@ -6,12 +6,17 @@ import org.bukkit.entity.Player;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.rule.PowerMockRule;
 
 import java.util.Collections;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Bukkit.class})
 public class SpigotLoggerTest {
 
     @Rule
