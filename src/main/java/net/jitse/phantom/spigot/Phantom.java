35438c8214052e4b0076f049f5950054a0ed2fbd
JitseB <Jitse@operamail.com>
2018-02-15 21:19:26 +0100

    Fix for previous commit.

--- src/main/java/net/jitse/phantom/spigot/Phantom.java
+++ src/main/java/net/jitse/phantom/spigot/Phantom.java
@@ -6,8 +6,6 @@ import net.jitse.phantom.spigot.storage.StorageManager;
 import net.jitse.phantom.spigot.utilities.files.Config;
 import net.jitse.phantom.spigot.utilities.logging.LogLevel;
 import net.jitse.phantom.spigot.utilities.logging.SpigotLogger;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Phantom extends JavaPlugin {
@@ -25,16 +23,10 @@ public class Phantom extends JavaPlugin {
     private SpigotLogger spigotLogger;
 
     @Override
-    public void onLoad() {
-        Bukkit.broadcastMessage(ChatColor.BLUE + "Loading Phantom.");
-    }
-
-    @Override
     public void onEnable() {
-        Bukkit.broadcastMessage(ChatColor.BLUE + "Enabling Phantom.");
         if (!setupConfigs(settingsConfig, messagesConfig)) {
             // If failed -> Stop enabling the plugin any further.
-            Bukkit.broadcastMessage(ChatColor.RED + "Phantom failed to create all config files.");
+            spigotLogger.log(LogLevel.FATAL, "Phantom failed to load all config files.");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
@@ -42,13 +34,12 @@ public class Phantom extends JavaPlugin {
         // Todo add config option for max log level.
         spigotLogger = new SpigotLogger("Phantom", LogLevel.DEBUG);
 
-        Bukkit.broadcastMessage(ChatColor.GREEN + "Phantom was enabled.");
+        spigotLogger.log(LogLevel.SUCCESS, "Enabled plugin.");
     }
 
     @Override
     public void onDisable() {
-        Bukkit.broadcastMessage(ChatColor.BLUE + "Disabling Phantom.");
-        Bukkit.broadcastMessage(ChatColor.RED + "Disabled plugin.");
+        spigotLogger.log(LogLevel.SUCCESS, "Disabled plugin.");
     }
 
     private boolean setupConfigs(Config... configs) {
